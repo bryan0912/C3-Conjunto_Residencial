525d703f70c35b2b57cc9f5061672a7dae63ea04
Bryan Ospina Henao <bryospina@gmail.com>
2020-07-30 22:34:21 -0500

    se trabajo en el proyecto

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// document.getElementById("btn-filtrar").addEventListener('click',(6) => {alert("filtrar")};
+
+
+document.getElementById("btn-guardar-inmueble").addEventListener('click', (e) =>{
+	if(document.getElementById("frm-mensualidad").reportValidity()){
+        let nombre, documento, genero, fecha_nac;
+    }else{
+        //alert("Error de validación de campos.")
+    }
+}
+);
+
+
+	
+    	
+$(document).ready((e) => {
+    $("#btn-adicionar").click((e) => {
+        $('#modal-adicionar').modal('toggle');
+    });
+});
